eb725163c4efbefd91e22f8b620e75a653d9201e
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2022-05-22 17:09:38 -0400

    corrected error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { RouterModule, Routes } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 
-const appRoutes: Routes = []
+const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
   { path: 'movies', component: MovieCardComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'prefix'},
