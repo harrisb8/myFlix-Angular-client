0dbc6773cefdd2298a6d373b1f5d8377358af4d2
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2022-05-22 15:44:29 -0400

    Added all movies/movie card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -18,4 +19,12 @@ export class AppComponent {
     width: '280px'
     });
   }
+
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px'
+    });
+  }
 }
+
+  
