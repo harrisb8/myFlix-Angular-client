0bd20161ca6570254769add9141ddd340280c006
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2022-05-22 21:38:02 -0400

    updated director and genre component code

--- src/app/director/director.component.ts
+++ src/app/director/director.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service'
 
 @Component({
   selector: 'app-director',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectorComponent implements OnInit {
 
-  constructor() { }
+  constructor(public fetchApiData: FetchApiDataService) { }
 
   ngOnInit(): void {
+    this.director();
   }
 
+  director(): void {
+    this.fetchApiData.getDirector().subscribe((resp: any) => {
+      this.director = resp;
+      console.log(this.director);
+      return this.director;
+  });
+ }
 }
