10a008217bc768f54fb76b3acebaa3313bfd35e8
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2022-06-15 13:06:02 -0400

    added new url for api

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -6,8 +6,8 @@ import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
 const apiUrl = //'https://movie-app-harris-02.herokuapp.com/';
- 
-'https://stormy-taiga-55813.herokuapp.com/';
+'https://angular-m.herokuapp.com/';
+//'https://stormy-taiga-55813.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
